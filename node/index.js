72eb60189a1dcd9b128c4acc2bcf4e497c0065d5
Alyson Rode <alysonrode001@gmail.com>
2023-05-07 00:35:55 -0300

    Revision

--- node/index.js
+++ node/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const config = {
-    host: 'db',
+    host: 'database_fullcycle',
     user: 'root',
     password: 'root',
     database: 'fullcycle-node-db'
